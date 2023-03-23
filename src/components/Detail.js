5e97801c264c903a51d646a91c57f386a3760d49
rhanziy <thdfksdl0029@gmail.com>
2023-03-23 13:06:36 +0900

    add fade-in/out with styled component

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,25 +1,39 @@
 import React from "react";
 import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 import { Nav } from "react-bootstrap";
 
 import Header from "./Header";
+
+
+let fadeIn = keyframes`
+    from {
+        opacity: 0;
+    } to {
+        opacity:1;
+    }
+`
+
 
 let FadeBox = styled.div`
     margin:30px;
     width:200px;
     height:200px;
     background-color: #6666FF;
+    animation: ${fadeIn} .5s ease-in;
 `
+
 
 let ContentBox = styled.div`
     padding:20px;
-    div { 
+    opacity:0;
+
+    &.in {
+        opacity:1;
         transition: 0.2s ease-in;
     }
 `
-
 
 let NavStyle = styled(Nav)`
     margin-top:50px;
@@ -29,53 +43,68 @@ function Detail() {
 
     let [ box, setBox ] = useState(true);  
     let [ tab, setTab ] = useState(0);
-    let [ fade, setFade ] = useState(false);
+    let [ fade, setFade ] = useState('');
 
     useEffect(()=>{
 
         let timer = setTimeout(()=> { 
             setBox(false); 
-        }, 1000);
-
+        }, 2000);
+ 
         return() => {
             clearTimeout(timer);
         }
 
     }, [])
+
     
-    
+    useEffect(()=>{
+        let timer = setTimeout(()=>{
+            setFade('in');
+        },100)
+        return()=>{
+            setFade('');
+            clearTimeout(timer);
+        }
+    }, [tab])
+
+
+
     return (
         <div>  
             <Header />
             <div>Detail Page</div>
             {
-                box ? <FadeBox>1초 뒤 사라짐</FadeBox> : null
+                box ? <FadeBox>2초 뒤 사라짐</FadeBox> : null
             }
             <NavStyle variant="tabs"  defaultActiveKey="link0">
                 <Nav.Item>
                 <Nav.Link 
+                    eventKey="link0"
                     onClick={()=> {setTab(0);}}>버튼1</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
                 <Nav.Link 
+                    eventKey="link1"
                     onClick={()=> {setTab(1);}}>버튼2</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
                 <Nav.Link  
+                    eventKey="link2"
                     onClick={()=> {setTab(2);}}>버튼3</Nav.Link>
                 </Nav.Item>
             </NavStyle>
-            <TabContent tab={ tab } />
+            <TabContent tab={ tab } fade={ fade } />
         </div>
     )
 }
 
 
-function TabContent({ tab }){
+function TabContent({ tab, fade }){
 
     
     return(
-        <ContentBox>
+        <ContentBox className={fade}>
             {[ 
                 <div>1번째 탭내용</div>, 
                 <div>2번째 탭내용</div>, 
